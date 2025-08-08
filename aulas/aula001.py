15d33ecbd7645e72d906b950ae215781cd43ad32
Miguel R M Lopes <miguelreism@hotmail.com>
2025-08-08 20:57:36 -0300

    Imprimindo dados
    Imprimindo dados e traduzindo colunas

---
+++ aulas/aula001.py
@@ -0,0 +1,34 @@
+import pandas as pd
+
+df = pd.read_csv("https://raw.githubusercontent.com/guilhermeonrails/data-jobs/refs/heads/main/salaries.csv")
+# arquivo .csv = dados tabulares, onde cada linha representa uma linha da tabela e os valores são separados por vírgulas
+
+linhas, colunas = df.shape
+
+# Dicionário de renomeação
+novos_nomes = {
+    'work_year': 'ano',
+    'experience_level': 'senioridade',
+    'employment_type': 'contrato',
+    'job_title': 'cargo',
+    'salary': 'salario',
+    'salary_currency': 'moeda',
+    'salary_in_usd': 'usd',
+    'employee_residence': 'residencia',
+    'remote_ratio': 'remoto',
+    'company_location': 'empresa',
+    'company_size': 'tamanho_empresa'
+}
+# Aplicando renomeação
+df.rename(columns=novos_nomes, inplace=True)
+
+print()
+print('Será impresso o DataFrame abaixo:')
+print(f'{linhas} linhas e {colunas} colunas.')# Exibe quantas linhas e colunas existem no DataFrame
+print()
+print(f'As colunas são: {df.columns}')  # Exibe os nomes das colunas do DataFrame
+print()
+print(df.info())  # Exibe informações sobre o DataFrame, como tipos de dados e contagem de valores não nulos
+print(df.describe())  # Exibe estatísticas descritivas do DataFrame
+print('\n Primeiras linhas do DataFrame:')
+print(df.head())  # Exibe as primeiras linhas do DataFrame

17e09ecc80e37c99312e8505fd15de8c959b8f22
Miguel R M Lopes <miguelreism@hotmail.com>
2025-08-08 21:29:15 -0300

    Mais traduções
    Mais traduções

--- aulas/aula001.py
+++ aulas/aula001.py
@@ -1,34 +1,72 @@
 import pandas as pd
 
-df = pd.read_csv("https://raw.githubusercontent.com/guilhermeonrails/data-jobs/refs/heads/main/salaries.csv")
-# arquivo .csv = dados tabulares, onde cada linha representa uma linha da tabela e os valores são separados por vírgulas
+# Carregar os dados
+url = "https://raw.githubusercontent.com/guilhermeonrails/data-jobs/refs/heads/main/salaries.csv"
+df = pd.read_csv(url)
 
 linhas, colunas = df.shape
 
 # Dicionário de renomeação
 novos_nomes = {
     'work_year': 'ano',
-    'experience_level': 'nivel_experiencia',
-    'employment_type': 'tipo_emprego',
+    'experience_level': 'senioridade',
+    'employment_type': 'contrato',
     'job_title': 'cargo',
     'salary': 'salario',
     'salary_currency': 'moeda',
-    'salary_in_usd': 'salario_usd',
+    'salary_in_usd': 'usd',
     'employee_residence': 'residencia',
-    'remote_ratio': 'taxa_remoto',
-    'company_location': 'localizacao_empresa',
+    'remote_ratio': 'remoto',
+    'company_location': 'empresa',
     'company_size': 'tamanho_empresa'
+}
+senioridade = {
+    'SE': 'senior',
+    'MI': 'pleno',
+    'EN': 'junior',
+    'EX': 'executivo'
+}
+contrato = {
+    'FT': 'integral',
+    'PT': 'parcial',
+    'CT': 'contrato',
+    'FL': 'freelancer'
+}
+remoto = {
+    0: 'presencial',
+    100: 'remoto',
+    50: 'hibrido'
+}
+tamanho_empresa = {
+    'L': 'grande',
+    'S': 'pequena',
+    'M':	'media'
+
 }
 # Aplicando renomeação
 df.rename(columns=novos_nomes, inplace=True)
+df['senioridade'] = df['senioridade'].replace(senioridade)
+df['contrato'] = df['contrato'].replace(contrato)
+df['remoto'] = df['remoto'].replace(remoto)
+df['tamanho_empresa'] = df['tamanho_empresa'].replace(tamanho_empresa)
 
 print()
 print('Será impresso o DataFrame abaixo:')
 print(f'{linhas} linhas e {colunas} colunas.')# Exibe quantas linhas e colunas existem no DataFrame
-print()
-print(f'As colunas são: {df.columns}')  # Exibe os nomes das colunas do DataFrame
+print(f'\nAs colunas são: {df.columns}')  # Exibe os nomes das colunas do DataFrame
 print()
 print(df.info())  # Exibe informações sobre o DataFrame, como tipos de dados e contagem de valores não nulos
 print(df.describe())  # Exibe estatísticas descritivas do DataFrame
 print('\n Primeiras linhas do DataFrame:')
 print(df.head())  # Exibe as primeiras linhas do DataFrame
+print()
+
+print(df['senioridade'].value_counts())  # Exibe a contagem de valores únicos na coluna 'senioridade'
+# SE = Senior, MI = Mid-level, EN = Entry-level, EX = Executive
+print()
+print(df['contrato'].value_counts())  # Exibe a contagem de valores únicos na coluna 'contrato'
+# FT = Full-time, PT = Part-time, CT = Contract, FL = Freelance
+print()
+print(df['remoto'].value_counts())  # Exibe a contagem de valores únicos na coluna 'remoto'
+print()
+print(df['tamanho_empresa'].value_counts())  # Exibe a contagem de valores únicos na coluna 'tamanho_empresa'
